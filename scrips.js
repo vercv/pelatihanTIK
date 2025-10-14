53dd033099efda4bccf7bde3f7a1111138863fdd
vercv <primamaulida1901@gmail.com>
2025-10-14 13:13:08 +0700

    scrips.js

--- scrips.js
+++ scrips.js
@@ -1,7 +1,3 @@
-//1. Pilih tombol berdasarkan ID-nya
-const tombolSapa = document.getElementById('sapaButton');
-//2. Tambahkan 'event listener' untuk aksi 'click'
-tombolSapa.addEventListener('click', function() {
-    // 3. Jalankan kode ini ketika tombol di-klik
-   alert('Halo! Terima kasih sudah berkunjung!");
+document.getElementById('sapaButton').addEventListener("click", () => {
+alert("Hello! Thank you for coming!")
 });
